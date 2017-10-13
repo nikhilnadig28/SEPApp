3ef3ce902284410760d528448e2b775aeb6822b6
Nikhil <nikhilnadig28@gmail.com>
2017-10-13 20:12:39 +0200

    Authorzation added.

--- SEPApp/Controllers/ManagerViewController.cs
+++ SEPApp/Controllers/ManagerViewController.cs
@@ -10,39 +10,39 @@ using SEPApp.Models;
 
 namespace SEPApp.Controllers
 {
-    [Authorize(Roles = "FinancialManager")]
-    public class FeedbackController : Controller
+    [Authorize(Roles = "ProductionManager, ServiceManager")]
+    public class ManagerViewController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
-        // GET: Feedback
+        // GET: ManagerView
         public ActionResult Index()
         {
-            return View(db.Events.Where(m => m.FinancialManagerComments == null).ToList());
+            return View(db.Events.Where(m => m.AdministrativeManagerApprove == true && m.SeniorCustomerApprove == true).ToList());
         }
 
-        // GET: Feedback/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Event @event = db.Events.Find(id);
-            if (@event == null)
-            {
-                return HttpNotFound();
-            }
-            return View(@event);
-        }
+        //// GET: ManagerView/Details/5
+        //public ActionResult Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Event @event = db.Events.Find(id);
+        //    if (@event == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(@event);
+        //}
 
-        //// GET: Feedback/Create
+        //// GET: ManagerView/Create
         //public ActionResult Create()
         //{
         //    return View();
         //}
 
-        //// POST: Feedback/Create
+        //// POST: ManagerView/Create
         //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         //[HttpPost]
@@ -59,38 +59,38 @@ namespace SEPApp.Controllers
         //    return View(@event);
         //}
 
-        // GET: Feedback/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Event @event = db.Events.Find(id);
-            if (@event == null)
-            {
-                return HttpNotFound();
-            }
-            return View(@event);
-        }
+        //// GET: ManagerView/Edit/5
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Event @event = db.Events.Find(id);
+        //    if (@event == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(@event);
+        //}
 
-        // POST: Feedback/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "EventId,EventName,EventType,ClientId,FromDate,ToDate,Preferences,Budget,SeniorCustomerApprove,AdministrativeManagerApprove,FinancialManagerComments")] Event @event)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(@event).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(@event);
-        }
+        //// POST: ManagerView/Edit/5
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include = "EventId,EventName,EventType,ClientId,FromDate,ToDate,Preferences,Budget,SeniorCustomerApprove,AdministrativeManagerApprove,FinancialManagerComments")] Event @event)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(@event).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(@event);
+        //}
 
-        // GET: Feedback/Delete/5
+        //// GET: ManagerView/Delete/5
         //public ActionResult Delete(int? id)
         //{
         //    if (id == null)
@@ -105,7 +105,7 @@ namespace SEPApp.Controllers
         //    return View(@event);
         //}
 
-        //// POST: Feedback/Delete/5
+        //// POST: ManagerView/Delete/5
         //[HttpPost, ActionName("Delete")]
         //[ValidateAntiForgeryToken]
         //public ActionResult DeleteConfirmed(int id)
